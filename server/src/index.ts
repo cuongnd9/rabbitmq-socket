6aafeac52d74bf973d9570e890b24fbaa0d3bd46
Cuong Duy Nguyen <103cuong@gmail.com>
2021-01-12 17:58:17 +0700

    chore: stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,6 @@ import { logger, globalOptions } from 'juno-js';
 import { config } from './components';
 import { connectToMongoDB } from './models';
 import { executeApp } from './app';
-import { executeConsumer } from './consumer';
-import { executeProducer } from './producer';
 
 globalOptions.environment = config.nodeEnv;
 
@@ -12,8 +10,6 @@ const main = async () => {
   try {
     await connectToMongoDB();
     executeApp();
-    executeProducer();
-    executeConsumer();
   } catch (e) {
     logger.error('Global error 🐛', e);
   }
